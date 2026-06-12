a9a3ee0cf9e04fa7ef87a8cce40188883ed92379
Adam Schiavone <adam8797@me.com>
2026-06-12 13:47:05 -0400

    new package versions

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PORT=5261
-
-echo "=== Listing services ==="
-grpcurl -plaintext localhost:$PORT list
-
-echo -e "\n=== Health check ==="
-grpcurl -plaintext localhost:$PORT grpc.health.v1.Health/Check
