61b475b86bdf104c69adacfa5f789e3a87e723f5
Adam Schiavone <adam8797@me.com>
2026-06-12 13:21:39 -0400

    Added basic project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using GrpcReflectionRepro.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+builder.Services.AddGrpcHealthChecks()
+    .AddCheck("self", () => Microsoft.Extensions.Diagnostics.HealthChecks.HealthCheckResult.Healthy());
+
+var app = builder.Build();
+
+app.MapGrpcService<GreeterService>();
+app.MapGrpcReflectionService();
+app.MapGrpcHealthChecksService();
+
+app.Run();
